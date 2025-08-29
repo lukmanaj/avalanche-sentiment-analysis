be7e7aa2cc7e9cc528d732bc9f1fe3821b4a9a23
Lukman Aliyu Jibril <aliyulukman94@gmail.com>
2025-08-29 01:25:50 +0100

    Update streamlit-app.py

--- streamlit-app.py
+++ streamlit-app.py
@@ -8,10 +8,7 @@ import plotly.express as px
 
 # Helper function to get dataset path
 def get_dataset_path():
-    # Get the current script directory
-    current_dir = os.path.dirname(os.path.abspath(__file__))
-    # Construct the path to the CSV file
-    csv_path = os.path.join(current_dir, "..", "..", "data", "customer_reviews.csv")
+    csv_path = "data/customer_reviews.csv"
     return csv_path
 
 
